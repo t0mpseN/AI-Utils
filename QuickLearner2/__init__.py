1205144d920bb3645e637aad4d2c3a3861f9863c
t0mpseN <pedrotbandel@gmail.com>
2025-05-19 18:35:56 -0300

    update

--- QuickLearner2/__init__.py
+++
@@ -1,9 +0,0 @@
-# quicklearner/__init__.py
-from .document_processor import load_documents, split_documents
-from .vector_store import create_vector_store
-from .ollama_integration import setup_llm
-from .rag_pipeline import create_rag_pipeline
-from .main import QuickLearner
-
-__all__ = ['load_documents', 'split_documents', 'create_vector_store', 
-           'setup_llm', 'create_rag_pipeline', 'QuickLearner']
--- QuickLearner2/ollama_integration.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import OllamaLLM
-
-def setup_llm(model_name):
-    # Initialize Ollama with your preferred model
-    return OllamaLLM(model=model_name)
